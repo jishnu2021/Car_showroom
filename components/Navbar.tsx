47d63efd05aff536e110d423c1295a0874c019ae
Jishnu Ghosh <jishnughosh2023@gmail.com>
2023-09-07 20:28:51 +0530

    second commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import CustomButton from "./CustomButton";
 const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
-      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+      <nav className="max-w-[1600px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
         <div className="flex justify-center items-center">
           <Link href="/">
             
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,9 @@ export interface CustomButtonProps{
     btnType?:"button" | "submit";
     handleClick?:MouseEventHandler<HTMLButtonElement>
 }
+
+export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+  }
+  
